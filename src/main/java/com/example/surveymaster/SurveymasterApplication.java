2f516a438b682919ad3c13faed65cd18cd573278
Dayanithi_E_K <dayanithiek.21cse@kongu.edu>
2025-02-07 11:19:02 +0530

    initial

---
+++ src/main/java/com/example/surveymaster/SurveymasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.surveymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveymasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveymasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/surveymaster/SurveymasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.surveymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveymasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
